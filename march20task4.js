9b8650780004a4410d7b703773b68a217f11cf05
priyadiv2001 <158546035+priyadiv2001@users.noreply.github.com>
2024-03-21 13:07:50 +0530

    Add files via upload

---
+++ march20task4.js
@@ -0,0 +1,125 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr=[4,2,7,1,9];
+// console.log(arr.sort())
+
+// let arr=[4,2,7,1,9]
+// let arr1=arr.sort()
+// console.log(arr1.reverse())
+
+// let arr=["apple","cherry","date",];
+// console.log(arr.sort())
+
+
+
+
+// let arr=[{ name: "Alice", age: 30 },
+//  { name: "John", age: 25 },
+//   { name: "Bob", age: 20 }]
+// console.log(arr.sort((a,b)=>b.age-a.age))
+
+let arr=[10,20,30,40,50];
+let arr1=arr.find((ele)=>{
+       return ele>25
+})
+console.log(arr1)
+
+
+
+// let arr=["apple","banana","orange","grape"]
+// let s="b"
+// let v=arr.find((ele)=>{
+//     return ele.startsWith (s)
+// // })
+// // console.log(v)
+
+// let arr=["apple","banana","grapes","promogranate"]
+// let s="p"
+// let v=arr.find((ele)=>{
+//     return ele.startsWith(s)
+// })
+// console.log(v)
+
+// let arr=[15,25,35,45,55]
+// let arr1=arr.findIndex((ele,ind)=>{
+//     if (ele%10==0)
+//     return ind;
+
+// })
+// console .log(arr1)
+
+// let arr=[3,7,8,12,15]
+// let k=arr.some((ele)=>{
+//     if(ele%2===0);
+//     return true
+// })
+
+// console.log(k)
+
+// const arr=[5,10,15,20,25];
+// let k=arr.reduce((acc,next)=>acc+next)
+// console.log(k)
+
+// const arr=["Hello", " ", "world", "!"];
+// let k=arr.reduce((acc,next)=>acc+next)
+// console.log(k)
+
+// let arr=[{ name: "John", age: 25 }, 
+
+// { name: "Alice", age: 30 },
+//  { name: "Bob", age: 20 }]
+
+// let arr1=arr.some((ele)=>{
+//     if(ele.age>18)
+//     return ele
+// })
+// console .log(arr1)
+
+// let arr=["apple", "banana", "grape", "kiwi"];
+// let s="b"
+// let v=arr.some((ele)=>{
+//     return ele.startsWith(s)
+// })
+// console.log(v)
+
+// let arr=[[1, 2], [3, 4], [5, 6]];
+// let k=arr.concat((ele)=>{
+//     return 
+// })
+// console.log(k)
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
